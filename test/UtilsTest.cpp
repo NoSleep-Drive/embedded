1361a9a644739192c2325fff51adb12d51d8954f
최하은 <haeun4345@gmail.com>
2025-05-18 21:05:29 +0900

    fix: 루프문 표준 c++ 문법으로 변경
    리눅스에서는 Microsoft의 확장 문법을 지원하지 않습니다.

--- test/UtilsTest.cpp
+++ test/UtilsTest.cpp
@@ -1,20 +1,19 @@
 #include "../include/Utils.h"
 
 int main() {
-
 	Utils test;
 	std::vector<cv::Mat> data = test.loadFramesFromRecentFolder();
 
 	std::string path = test.createSleepinessDir("test1234");
 	int count = 0;
-	for each (cv::Mat var in data)
-	{
+
+	for (const cv::Mat& var : data) {
 		std::string name = std::to_string(count++) + ".jpg";
 		test.saveFrameToSleepinessFolder(var, name);
 		test.saveFrameToCurrentFrameFolder(var, name);
 	}
 
-	//test.removeFolder(path);
+	// test.removeFolder(path);
 
 	return 0;
 }
