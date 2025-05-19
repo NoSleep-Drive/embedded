4524bf3b719c8f585b9af74a780ab37e2d77e148
Subin <rla0010101@gmail.com>
2025-05-19 13:45:44 +0900

    refactor[#13]: 모니터링 쓰레드 안정성 개선

--- include/DBThreadMonitoring.h
+++ include/DBThreadMonitoring.h
@@ -7,28 +7,35 @@
 #include <unordered_set>
 #include <condition_variable>
 #include <string>
+#include <atomic>
+#include <memory>
 
 class DBThread;
 
 class DBThreadMonitoring {
 private:
     std::atomic<bool> isDBThreadRunning = false;
-    std::queue<DBThread*> threadQueue;
+    std::atomic<bool> terminate = false;
+
+    std::queue<std::shared_ptr<DBThread>> threadQueue;
     std::unordered_set<std::string> activeThreadKeys;
     std::mutex queueMutex;
     std::condition_variable condition;
+    std::thread monitoringThread;
 
     void processThreadQueue();
     std::string generateThreadKey(const std::string& deviceUid, const std::string& folderPath);
 
 public:
     DBThreadMonitoring();
+    ~DBThreadMonitoring();
     void startDBMonitoring();
-    void addDBThread(DBThread* thread);
+    void addDBThread(std::shared_ptr<DBThread> thread);
+    void removeActiveThread(const std::string& key);
 
     bool getIsDBThreadRunning() const;
     void setIsDBThreadRunning(bool value);
-    void removeActiveThread(const std::string& key);
+    
 };
 
 #endif
--- src/DBThreadMonitoring.cpp
+++ src/DBThreadMonitoring.cpp
@@ -10,44 +10,55 @@ DBThreadMonitoring::DBThreadMonitoring() {
     startDBMonitoring();
 }
 
+DBThreadMonitoring::~DBThreadMonitoring() {
+    terminate = true;
+    condition.notify_all();
+    if (monitoringThread.joinable()) {
+        monitoringThread.join();
+    }
+}
+
 void DBThreadMonitoring::startDBMonitoring() {
-    std::thread([this] {
+    monitoringThread = std::thread([this] {
         processThreadQueue();
-        }).detach();
+    });
 }
 
 void DBThreadMonitoring::processThreadQueue() {
     std::unique_lock<std::mutex> lock(queueMutex);
 
-    while (true) {
+    while (!terminate) {
         condition.wait(lock, [this] {
-            return !threadQueue.empty();
-            });
+            return terminate || !threadQueue.empty();
+         });
 
         while (!threadQueue.empty()) {
-            DBThread* thread = threadQueue.front();
+            auto thread = threadQueue.front();
             threadQueue.pop();
-
             lock.unlock();
-            std::thread([thread, this] {
-                thread->sendDataToDB();
-                removeActiveThread(generateThreadKey(thread->getDeviceUid(), thread->getFolderPath()));
+
+            std::thread([ptr = thread, this] {
+                try {
+                    ptr->sendDataToDB();
+                }
+                catch (const std::exception& e) {
+                    std::cerr << "DBThread 예외: " << e.what() << std::endl;
+                }
+                removeActiveThread(generateThreadKey(ptr->getDeviceUid(), ptr->getFolderPath()));
                 }).detach();
-            lock.lock();
+                lock.lock();
         }
     }
 }
 
-void DBThreadMonitoring::addDBThread(DBThread* thread) {
+void DBThreadMonitoring::addDBThread(std::shared_ptr<DBThread> thread) {
     std::unique_lock<std::mutex> lock(queueMutex);
     std::string threadKey = generateThreadKey(thread->getDeviceUid(), thread->getFolderPath());
 
     if (activeThreadKeys.find(threadKey) == activeThreadKeys.end()) {
         activeThreadKeys.insert(threadKey);
         threadQueue.push(thread);
-        lock.unlock();
         condition.notify_one();
-        lock.lock();
         std::cout << "DBThread 추가됨, 큐 크기: " << threadQueue.size() << std::endl;
     }
     else {
@@ -58,7 +69,6 @@ void DBThreadMonitoring::addDBThread(DBThread* thread) {
 void DBThreadMonitoring::removeActiveThread(const std::string& key) {
     std::unique_lock<std::mutex> lock(queueMutex);
     activeThreadKeys.erase(key);
-    lock.unlock();
     condition.notify_all();
 }
 
