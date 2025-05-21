f12c2827bb072904adda724a8cf68135c9e069e6
최하은 <haeun4345@gmail.com>
2025-05-21 17:21:09 +0900

    fix[#22]: 밀리초 포맷 오류 수정

--- src/FirmwareManager.cpp
+++ src/FirmwareManager.cpp
@@ -323,7 +323,9 @@ bool FirmwareManager::processSingleFrame() {
 	auto now = std::chrono::system_clock::now();
 	auto in_time_t = std::chrono::system_clock::to_time_t(now);
 	std::stringstream ss;
-	ss << std::put_time(std::localtime(&in_time_t), "%Y%m%d_%H%M%S_%03d");
+	ss << std::put_time(std::localtime(&in_time_t), "%Y%m%d_%H%M%S");
+	auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()) % 1000;
+	ss << '_' << std::setfill('0') << std::setw(3) << ms.count();
 	std::string timestamp = ss.str();
 
 	// 프레임 저장
